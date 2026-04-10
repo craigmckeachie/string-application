bd84c7333ee1d9cc691afc22a1379e989583ba77
Craig McKeachie <craigdmckeachie@gmail.com>
2026-04-10 14:53:04 -0400

    example: If

---
+++ src/main/java/com/pluralsight/IfApp.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class IfApp {
+
+    public static void main(String[] args) {
+        int magicNumber = 99;
+        boolean condition = magicNumber < 99 ;
+
+        if (condition) {
+            System.out.println("condition is true");
+            System.out.println("test");
+            System.out.println("another thing");
+        }
+    }
+}
