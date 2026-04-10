2d66eeeb24c74438f988e9bbcecb45dfe2bb4a89
Craig McKeachie <craigdmckeachie@gmail.com>
2026-04-10 16:52:33 -0400

    example: Pseudo-code

---
+++ src/main/java/com/pluralsight/Program.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+public class Program {
+
+    public static void main(String[] args) {
+//        Prompt the user for the size of the sandwich (1 or 2):
+//        a. 1: Regular: base price $5.45
+//        b. 2: Large: base price $8.95
+        //print the message "Please choose a sandwich size: "
+        //print "(R)egular: base price $5.45"
+        //print "(L)arge: base price $8.95"
+
+        //read the user input as a string R or L and store it as sandwich size
+
+
+//        Prompt the user for their age
+//        print the message "Please enter your age: "
+//       read the user input as an int  and store it as age
+
+//       create variable to hold cost of sandwich it should be a double
+
+//        if Student (17 years old or younger) then
+//          give a 10% discount  (cost = cost *.90)
+//        if Seniors (65 years old or older) –
+//           give a 20% discount (cost = cost * .80)
+
+
+//        Display the cost of the sandwich to the screen
+        //print the message "Your sandwich will cost $0.00"
+    }
+}
